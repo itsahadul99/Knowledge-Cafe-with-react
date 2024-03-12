b38ec34a37345768cb042d37250a0ac7988e42b0
itsahadul99 <itsahadul999@gmail.com>
2024-03-12 23:16:40 +0600

    fetch the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import Blogs from "./components/Blogs/Blogs"
+import Header from "./components/Header/Header"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-5xl bg-red-400">Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
